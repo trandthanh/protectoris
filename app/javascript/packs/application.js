376924d601fe3ed3efc8d654260e8041f473d34d
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-06-30 14:02:20 +0200

    Finished Home Banner for Desktop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,8 @@ import "bootstrap";
 
 import { table } from '../table';
 const comparison = document.querySelector('.comparison-table');
-if (comparison) { table() }
+if (comparison) { table() };
+
+import { scroll } from '../scroll';
+const homescroll = document.querySelector('.scroll-icon');
+if (homescroll) { scroll() };
---
+++ app/javascript/scroll.js
@@ -0,0 +1,13 @@
+function scroll() {
+
+  const element = document.getElementById('click-item');
+  const el = document.querySelector('.home-features-wrapper');
+
+  element.addEventListener("click", (event) => {
+    console.log('hello');
+    el.scrollIntoView(true);
+  })
+}
+
+export { scroll };
+
